1af2b85ee3f597c093d561d5c308126ef78e4da8
Your Name <you@example.com>
2025-12-03 09:28:18 +0100

    Add project files.

---
+++ CManager.Domain/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CManager.Domain
+{
+    public class Customer
+    {
+        public Guid Id { get; set; }     
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        // Adress
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+
+
+    }
+}
