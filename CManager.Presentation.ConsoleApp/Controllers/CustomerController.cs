f04997bb9cf7cd27dc475bb670dad8aeae89ad11
Your Name <you@example.com>
2025-12-05 13:39:52 +0100

    Add  Menu loop and Dialog method stubs to CustomerController

--- CManager.Presentation.ConsoleApp/Controllers/CustomerController.cs
+++ CManager.Presentation.ConsoleApp/Controllers/CustomerController.cs
@@ -18,6 +18,69 @@ namespace CManager.Presentation.ConsoleApp.Controllers
             _service = service;
         }
 
+        public void Start()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("-----Kundhantering-----");
+                Console.WriteLine("1. Skapa kund");
+                Console.WriteLine("2. Visa kunder");
+                Console.WriteLine("3. Visa specifik kund");
+                Console.WriteLine("4. Ta bort kund");
+                Console.WriteLine("5. Avsluta");
+                Console.Write("Välj ett alternativ: ");
+
+                string choice = Console.ReadLine()!;
+
+                switch (choice)
+                {
+                    case "1":
+                      //  CreateCustomerDialog();
+                            break;
+
+                    case "2":
+                        //ShowAllCustomersDialog();
+                        break;
+
+                    case "3":
+                        //ShowSpecificCustomerDialog();
+                        break;
+
+                    case "4":
+                       // DeleteCustomerDialog();
+                        break;
+
+                    case "5":
+                        return;
+
+                    default:
+                        {
+                            Console.WriteLine("Fel val, försök igen");
+                            Console.ReadKey();
+                            break;
+                        }
+
+                }
+                  
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
