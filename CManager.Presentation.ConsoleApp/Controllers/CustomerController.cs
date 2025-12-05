cde15c56115291324fe5680342bc52abaf776d5d
Your Name <you@example.com>
2025-12-05 13:04:10 +0100

    Implement Dependency Injection  constructor in CustomerController to inject ICustomerService

--- CManager.Presentation.ConsoleApp/Controllers/CustomerController.cs
+++ CManager.Presentation.ConsoleApp/Controllers/CustomerController.cs
@@ -11,7 +11,12 @@ namespace CManager.Presentation.ConsoleApp.Controllers
 {
     public class CustomerController
     {
-        
+        private readonly ICustomerService _service;
+
+        public CustomerController(ICustomerService service)
+        {
+            _service = service;
+        }
 
 
     }
