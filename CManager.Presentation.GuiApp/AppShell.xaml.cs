7f91ebdaf51f42c482ec315a51c4e6840e692b89
Your Name <you@example.com>
2025-12-07 13:53:30 +0100

    Initial backup before fix.

---
+++ CManager.Presentation.GuiApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CManager.Presentation.GuiApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CManager.Presentation.GuiApp/Views/CustomersPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace CManager.Presentation.GuiApp.Views;
+
+public partial class CustomersPage : ContentPage
+{
+	public CustomersPage()
+	{
+		InitializeComponent();
+	}
+}
